b46bc7423a82214ada939fa02d9f004225ecaf44
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-09-12 02:22:45 -0300

    Projeto inicial

---
+++ src/screens/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const profile = {
+  "picture": "https://secure.gravatar.com/avatar/f50a9db56e231198af3507f10b5d5491?d=mm",
+  "email": "rafael.fuzifaru@gmail.com",
+  "first_name": "Rafael",
+  "last_name": "Fuzifaru Cianci",
+  "phone": "(48) 99660-6666",
+  "gender": 1,
+  "birthday": "1993-04-27T00:00:00-03:00",
+  "linkedin": "https://www.linkedin.com/in/rafaelcianci",
+  "github": "http://github.com/rafacianci",
+  "address": {
+    "Street": "",
+    "ZipCode": "",
+    "Number": "",
+    "ComplementaryAddress": ""
+  },
+  "language": ["Português - PT", "Inglês - EN", "Japonês - JA"],
+  "name": "Rafael Fuzifaru Cianci"
+}
+
+export default class Profile extends React.PureComponent {
+
+  render() {
+
+    return (
+      <View></View>
+    );
+  }
+}
