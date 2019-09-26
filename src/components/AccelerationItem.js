5d239e8077cb09a2c7cce4e4eb3eb131b60985ad
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-09-25 22:41:01 -0300

    Ajuste no layout do perfil

--- src/components/AccelerationItem.js
+++ src/components/AccelerationItem.js
@@ -17,24 +17,24 @@ export default function AccelerationItem({ item }) {
 
         <View className={"contact-content"} style={[styles.userContainer, { opacity: 1 }]}>
           <Text className={"contact-label"} style={styles.label}>Linkedin:</Text>
-          <Text className={"contact-value"} style={styles.input}>{profile.linkedin}</Text>
+          <Text className={"contact-value"} style={[styles.text, styles.mBottom]}>{profile.linkedin}</Text>
 
           <Text className={"contact-label"} style={styles.label}>GitHub:</Text>
-          <Text className={"contact-value"} style={styles.input}>{profile.github}</Text>
+          <Text className={"contact-value"} style={[styles.text, styles.mBottom]}>{profile.github}</Text>
         </View>
 
         <View className={"contact-content"} style={[styles.userContainer, { opacity: 1 }]}>
           <Text className={"contact-label"} style={styles.label}>E-mail:</Text>
-          <Text className={"contact-value"} style={styles.input}>{profile.email}</Text>
+          <Text className={"contact-value"} style={[styles.text, styles.mBottom]}>{profile.email}</Text>
 
           <Text className={"contact-label"} style={styles.label}>Celular:</Text>
-          <Text className={"contact-value"} style={styles.input}>{profile.phone}</Text>
+          <Text className={"contact-value"} style={[styles.text, styles.mBottom]}>{profile.phone}</Text>
 
           <Text className={"contact-label"} style={styles.label}>Data de Nascimento:</Text>
-          <Text className={"contact-value"} style={styles.input}>{moment(new Date(profile.birthday)).format("DD/MM/YYYY")}</Text>
+          <Text className={"contact-value"} style={[styles.text, styles.mBottom]}>{moment(new Date(profile.birthday)).format("DD/MM/YYYY")}</Text>
 
           <Text className={"contact-label"} style={styles.label}>Sexo:</Text>
-          <Text className={"contact-value"} style={styles.input}>{profile.gender == 1 ? 'Masculino': 'Feminino'}</Text>
+          <Text className={"contact-value"} style={[styles.text, styles.mBottom]}>{profile.gender == 1 ? 'Masculino': 'Feminino'}</Text>
 
           <Text className={"contact-label"} style={styles.label}>Idioma:</Text>
           <FlatList
@@ -50,45 +50,49 @@ export default function AccelerationItem({ item }) {
 
 const styles = StyleSheet.create({
   container:{
-    margin:10
-  },
-  label:{
-    fontSize:12,
-    color:'#FFFFFF'
-  },
-  input:{
-    fontSize:12,
-    color:'#FFFFFF',
-    marginBottom:10
-  },
-  userContainer:{
-    backgroundColor:'#111111',
-    marginTop:15,
-    padding:10
-  },
-  language:{
-    fontSize:12,
-    color:'#FFFFFF',
-    backgroundColor:'#808080',
-    marginTop:5,
-    marginRight:10,
-    marginBottom:5,
-    padding:8,
-    borderRadius:20
-  },
-  avatar:{
-    width: 45, 
-    height: 45,
-    borderRadius:22
-  },
-  name: {
-    color:'#7800FF',
-    fontSize:20,
-    paddingLeft:10
+    flex: 1
   },
   userHeader:{
-    margin:10,
-    flexDirection:'row',
     alignItems: 'center',
+    flexDirection: 'row',
+    padding: 16
   },
+  avatar:{
+    borderRadius: 22,
+    height: 45,
+    width: 45
+  },
+  name: {
+    color: '#7800ff',
+    fontSize: 20,
+    paddingLeft: 16
+  },
+  userContainer:{
+    backgroundColor: '#000',
+    borderRadius: 2,
+    margin: 16,
+    padding: 16,
+    marginTop: 0
+  },
+  label:{
+    color: '#FFFFFF',
+    fontSize: 11
+  },
+  text:{
+    color: '#FFFFFF',
+    fontSize: 14
+  },
+  language:{
+    backgroundColor: '#666',
+    borderRadius: 50,
+    marginRight: 16,
+    marginTop: 8,
+    color: '#FFFFFF',
+    fontSize: 12,
+    padding: 5,
+    paddingHorizontal: 10
+  },
+  mBottom: {
+    marginBottom: 16
+  }
 });
